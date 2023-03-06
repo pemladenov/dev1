f83f8c61219423bb104df500f5c09a1cd63434ac
501a3594-8e42-4455-8bbc-35f968b37e68 <NDgxNjUyMDE2ODUwOiHbsgZ9LlpX9i126aSk+dBA6WQ6>
2023-03-06 13:32:45 +0100

    switched languages

--- myproject/src/graph.py
+++ myproject/src/graph.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 x = [1, 2, 3, 4, 5]
 y = [2, 4, 6, 8, 10]
 
-# Create plot
+# Erstellen vom plot
 plt.plot(x, y)
 
-# Add labels and title
-plt.xlabel('X axis label')
-plt.ylabel('Y axis label')
-plt.title('Simple Line Plot')
+#labels
+plt.xlabel('X axis')
+plt.ylabel('Y axis')
+plt.title('Simpler Plot')
 
 # Show plot
 plt.show()
